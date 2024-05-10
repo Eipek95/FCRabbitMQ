ba328ca7f38f5cc24d4a65c377b9fb1f9a4260c4
Eipek95 <eipek1995@gmail.com>
2024-05-10 15:35:23 +0300

    edit

--- FCRabbitMQ.publisher/Program.cs
+++ FCRabbitMQ.publisher/Program.cs
@@ -1,27 +1,51 @@
 ﻿using RabbitMQ.Client;
 using System.Text;
 
-var factory = new ConnectionFactory();
-factory.Uri = new Uri("amqps://yunsubup:eOTyDZ80baa2afV_AR_VS-O16GCeYKlq@fish.rmq.cloudamqp.com/yunsubup");
 
-
-
-using (var connection = factory.CreateConnection())
+public enum LogNames
 {
-    var channel = connection.CreateModel();
-    channel.QueueDeclare("hello-queue", true, false, false);
-
-    //rabbitmq ya mesajlar byte dizisi olarak gönderilebildiği için herşey gönderebiliriz
-
-    Enumerable.Range(1, 50).ToList().ForEach(x =>
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Info = 4
+}
+public class Program
+{
+    private static void Main(string[] args)
     {
-        string message = $"Message [{x}]";
-        var messageBody = Encoding.UTF8.GetBytes(message);
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri("amqps://yunsubup:eOTyDZ80baa2afV_AR_VS-O16GCeYKlq@fish.rmq.cloudamqp.com/yunsubup");
 
-        channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
-        Console.WriteLine($"Mesaj gönderilmişltir. {message} {DateTime.Now}");
-    });
 
-    Console.ReadLine();
 
+        using (var connection = factory.CreateConnection())
+        {
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
+
+
+            Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+            {
+                var routeKey = $"route-{x}";
+                var queueName = $"direct-queue-{x}";
+                channel.QueueDeclare(queueName, true, false, false);//kuyruk ad-durable-farklı channelardan bağlanabileyim-otomatik silinmesin yani subscribe yoksa mesaj kuyrukta beklesin
+
+                channel.QueueBind(queueName, "logs-direct", routeKey, null);
+            });
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                LogNames log = (LogNames)new Random().Next(1, 5);
+
+
+                string message = $"log-type: [{log}]";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+                var routeKey = $"route-{log}";
+                channel.BasicPublish("logs-direct", routeKey, null, messageBody);
+                Console.WriteLine($"Log gönderilmişltir. {message} {DateTime.Now}");
+            });
+
+            Console.ReadLine();
+
+        }
+    }
 }
--- RabbitMQ.subscriber/Program.cs
+++ RabbitMQ.subscriber/Program.cs
@@ -7,27 +7,22 @@ factory.Uri = new Uri("amqps://yunsubup:eOTyDZ80baa2afV_AR_VS-O16GCeYKlq@fish.rm
 using (var connection = factory.CreateConnection())
 {
     var channel = connection.CreateModel();
-    // channel.QueueDeclare("hello-queue", true, false, false);//bu kod bu tarafta olması herhangi bir hata teşkil etmez.oladabilir olmayadabilir.publisher ve subscriber tarafında aynı kuyruk oluşuyorsa içinde geçen parametrelre aynı olmalı.publish tarafında bu kuyruk oldugun eminsek burada tekrar oluşturmaya gerek yok.
-
-
-    //her bir subscribe 1 mesaj gönderir
     channel.BasicQos(0, 1, false);
-
-
-
     var consumer = new EventingBasicConsumer(channel);
+    var queueName = "direct-queue-Critical"; //rabbitmq sitesinden baktım
 
-    //channel.BasicConsume("hello-queue", true, consumer);//mesajlar doğru da işlense yanlışta işlense direk siler true olunca kuyruktan.mesaj iletildiği an kuyruktan siler
-    channel.BasicConsume("hello-queue", false, consumer);//mesajları hemen silmez rabbitmq.haber bekler
+
+    channel.BasicConsume(queueName, false, consumer);
+    Console.WriteLine("Loglar dinleniyor....");
 
     consumer.Received += (object sender, BasicDeliverEventArgs e) =>
     {
         var message = Encoding.UTF8.GetString(e.Body.ToArray());
 
-        Thread.Sleep(1500);//her bir mesaj 1.5 saniyede işlenecek gibi
-        Console.WriteLine(message);
-
-        channel.BasicAck(e.DeliveryTag, false);//rabbitmq ye bilgilerndirme yapar mesajları silmesi için
+        Thread.Sleep(1500);
+        Console.WriteLine("Gelen Mesaj: " + message);
+        File.AppendAllText("log-critical.txt", message + "\n");
+        channel.BasicAck(e.DeliveryTag, false);
     };
     Console.ReadLine();
 
