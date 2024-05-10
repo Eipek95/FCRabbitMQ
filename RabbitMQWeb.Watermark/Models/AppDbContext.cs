8e2f026e2f895a8eb6ccab29d9f4eb372ad5c1e4
Eipek95 <eipek1995@gmail.com>
2024-05-10 21:13:19 +0300

    edit

---
+++ RabbitMQWeb.Watermark/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RabbitMQWeb.Watermark.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            //startup tarafında options doldurcağımızdan dolayı base'e options verdik
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
